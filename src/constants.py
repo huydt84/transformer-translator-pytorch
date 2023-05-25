14b8ee6d9053967c2a0ee3b9a10051b551cbfa71
huydt84 <viethuy061002@gmail.com>
2023-05-25 00:44:26 -0700

    combine modules

--- src/constants.py
+++ src/constants.py
@@ -5,6 +5,7 @@ DATA_DIR = 'data'
 SP_DIR = f'{DATA_DIR}/sp'
 SRC_DIR = 'src/envi'
 TRG_DIR = 'trg/envi'
+ONNX_DIR = 'onnx_test'
 SRC_RAW_DATA_NAME = 'raw_data.src'
 TRG_RAW_DATA_NAME = 'raw_data.trg'
 SRC_TRAIN_NAME = 'train.en'
@@ -13,7 +14,7 @@ SRC_TEST_NAME = 'test.en'
 TRG_TRAIN_NAME = 'train.vi'
 TRG_VALID_NAME = 'dev.vi'
 TRG_TEST_NAME = 'test.vi'
-# NUM_TRAIN = 50000
+NUM_TRAIN = 10000
 
 # Parameters for sentencepiece tokenizer
 pad_id = 0
@@ -29,6 +30,7 @@ model_type = 'bpe'
 
 # Parameters for Transformer & training
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+# device = 'cpu'
 learning_rate = 1e-4
 betas = (0.9, 0.98)
 eps = 1e-9
@@ -37,7 +39,7 @@ seq_len = 256
 num_heads = 8
 num_layers = 3
 d_model = 512
-d_ff = 2048
+d_ff = 1024
 d_k = d_model // num_heads
 drop_out_rate = 0.1
 num_epochs = 1
